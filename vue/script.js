19b5ea762c754c60e6f9074fe00a29c8c0e948b4
Emanuele <cannone.emanuele@gmail.com>
2021-01-26 19:19:19 +0100

    finito

--- vue/script.js
+++ vue/script.js
@@ -1,23 +1,35 @@
 var app = new Vue({
-    el: '.container',
+    el: 'main',
     data:{
         indiceNumerico: 0,
+        colore: 'blu',
         src: [
             'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/2000px-Flag_of_Spain.svg.png',
             'https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg',
             'https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg',
             'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1200px-Flag_of_Italy.svg.png',
             'https://upload.wikimedia.org/wikipedia/commons/0/08/Flag_of_Switzerland_%28Pantone%29.svg'
+        ],
+        alt: [
+            'spagna',
+            'francia',
+            'germania',
+            'italia',
+            'svizzera'
         ]
+        
     },
     methods:{
         successivo(){
             this.indiceNumerico++;
-            if (this.indiceNumerico == this.src.length - 1) this.indiceNumerico = 0;
+            if (this.indiceNumerico == this.src.length) this.indiceNumerico = 0;
         },
         precedente(){
             this.indiceNumerico--;
             if (this.indiceNumerico < 0) this.indiceNumerico = this.src.length - 1;
+        },
+        scegliBandiera(i){
+            this.indiceNumerico = i;
         }
     },
     
